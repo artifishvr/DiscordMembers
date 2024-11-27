f37b0d3e6bbaf1f1ddcc6a4bce17677c5a16567a
Arti <59352535+artifishvr@users.noreply.github.com>
2024-11-27 14:13:22 -0800

    the code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,81 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { load } from "cheerio";
+
+interface Env {
+  DISCORDMEMBERS: KVNamespace;
+  WEBHOOK: string;
+  INVITE: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async scheduled(
+    controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ) {
+    const WEBHOOK_URL = env.WEBHOOK;
+
+    const prevMemberCount = await env.DISCORDMEMBERS.get("members");
+    try {
+      const response = await fetch(env.INVITE);
+      const html = await response.text();
+
+      const $ = load(html);
+
+      const description = $('meta[name="description"]').attr("content");
+
+      const memberMatch = description?.match(/(\d+)\s+members/);
+
+      if (!memberMatch) {
+        await fetch(WEBHOOK_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content: `No match found.`,
+          }),
+        });
+        throw new Error("No member count found");
+      }
+      const memberCount = memberMatch ? memberMatch[1] : "0";
+
+      if (prevMemberCount !== null) {
+        if (memberCount != prevMemberCount) {
+          await fetch(WEBHOOK_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              content: `member count changed to ${memberCount} <@532053122017787924>`,
+            }),
+          });
+        } else {
+          await fetch(WEBHOOK_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              content: `member count unchanged: ${memberCount}`,
+            }),
+          });
+        }
+      }
+
+      // Store in KV
+      await env.DISCORDMEMBERS.put("members", memberCount);
+    } catch (error) {
+      console.error("Error fetching Discord members:", error);
+      await fetch(WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: `i failed.`,
+        }),
+      });
+    }
+  },
+};
