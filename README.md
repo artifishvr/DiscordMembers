a4b13739f6cf625a4a80ddd7a6d8e25ae52fbee6
Arti <59352535+artifishvr@users.noreply.github.com>
2024-11-28 03:51:23 -0800

    there's an api for this

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
     <img src="https://skillicons.dev/icons?i=workers,discord,pnpm" />
 </a>
 
-Works without a bot, uses cheerio on cloudflare workers. Uses a KV store to store the member count, and a cron trigger.
+Works without a bot, runs on cloudflare workers. Uses a KV store to store the member count, and a cron trigger.
 
 ## Environment Variables
 
 - `WEBHOOK`: Webhook URL to post to
-- `INVITE`: URL to invite page
+- `INVITE`: Invite ID (don't include discord.gg/)
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
-import { load } from "cheerio";
+import { ofetch } from "ofetch";
 
 export default {
   async scheduled(env: Env) {
     const WEBHOOK_URL = env.WEBHOOK;
+    const INVITE = env.INVITE;
 
-    const prevMemberCount = await env.DISCORDMEMBERS.get("members");
     try {
-      const html = await fetch(env.INVITE).then((res) => res.text());
+      const prevMemberCount = await env.DISCORDMEMBERS.get("members");
+      const inviteData = await ofetch(
+        `https://discord.com/api/v10/invites/${INVITE}?with_counts=true`
+      ).then((res) => res.json());
 
-      const $ = load(html);
-
-      const description = $('meta[name="description"]').attr("content");
-
-      const memberMatch = description?.match(/(\d+)\s+members/);
-
-      if (!memberMatch) {
-        await fetch(WEBHOOK_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            content: `No match found.`,
-          }),
-        });
-        throw new Error("No member count found");
-      }
-      const memberCount = memberMatch ? memberMatch[1] : "0";
+      const memberCount = inviteData.approximate_member_count;
 
       if (prevMemberCount !== null) {
         if (memberCount != prevMemberCount) {
